2ed7facde098f43cec0fbcb71e3c5475896c75ad
mikeda2501 <mikeda2501@gmail.com>
2017-09-06 17:33:52 +0900

    fix

---
+++ assets/html/post.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang=ja>
+    <head>
+        <meta charset = "utf-8">
+        <title>Photo-Manager</title>
+        <!--index.cssのリンクタブ-->
+        <link href="assets/css/post.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <!--###ヘッダ###ー-->
+        <header>
+            <div class="header-box">
+                <!---alt属性は写真の属性を表す、特に意味のないときは""と書いておく--->
+                <a class="logo" id="logo" href="index.php"><img src="assets/img/post-logo02.png" alt=""></a>
+                <!--のちにアイコンをPhoto-ManegerとPostで分ける-->
+<!--                <a class="logo" id="logo" href="index.php"><img src="assets/img/post.png" alt=""></a>-->
+                <a class="acount-icon" href="index.php"><img src="assets/img/icon.png"></a>
+            </div>
+        </header>
+        <!--###メイン###-->
+        <div class = "main">
+            <div class="container">
+            </div>
+        </div>
+        <!--###フッタ###ー-->
+        <footer id="footer">
+        </footer>
+    </body>
+</html>
