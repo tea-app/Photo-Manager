b80f3bdbdb60dab543ee94ff5d1605a94fed27e0
mikeda2501 <mikeda2501@gmail.com>
2017-09-07 01:38:42 +0900

    fix post.php

--- assets/html/post.php
+++ assets/html/post.php
@@ -11,10 +11,10 @@
         <header>
             <div class="header-box">
                 <!---alt属性は写真の属性を表す、特に意味のないときは""と書いておく--->
-                <a class="logo" id="logo" href="index.php"><img src="assets/img/post-logo02.png" alt=""></a>
+                <a class="logo" id="logo" href="../html/index.php"><img src="../img/post-logo02.png" alt=""></a>
                 <!--のちにアイコンをPhoto-ManegerとPostで分ける-->
 <!--                <a class="logo" id="logo" href="index.php"><img src="assets/img/post.png" alt=""></a>-->
-                <a class="acount-icon" href="index.php"><img src="assets/img/icon.png"></a>
+                <a class="acount-icon" href="../html/index.php"><img src="../img/icon.png"></a>
             </div>
         </header>
         <!--###メイン###-->
